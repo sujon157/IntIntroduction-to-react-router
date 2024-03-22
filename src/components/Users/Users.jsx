e42b3933eb0b4c72f3256f6c1e0514e0bcf9d6d1
sujon157 <riponahamed1579@gmail.com>
2024-03-22 09:52:58 +0600

    final commit

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,16 +3,16 @@ import User from "../User/User";
 import './Users.css'
 
 const Users = () => {
-    const users= useLoaderData();
-    console.log(users);
+    const users = useLoaderData();
+
     return (
         <div >
             <h2 className="text-3xl font-bold">Our users: {users.length}</h2>
 
             <div className="users">
-              {
-                  users.map(user => <User key={user.id} user={user}></User>)
-              }
+                {
+                    users.map(user => <User key={user.id} user={user}></User>)
+                }
             </div>
         </div>
     );
